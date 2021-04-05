a9d5b6695cb3122ba3020eb608e3886ac7efb5e5
nicasalazar <60679947+nicasalazar@users.noreply.github.com>
2021-04-04 22:53:14 -0700

    create new functions for part 2

---
+++ JS/app.js
@@ -0,0 +1,5 @@
+/* Functions to add */
+
+/*Function 1: create.onClick function which creates a new note*/
+/*Function 2: save.onClick function which saves the note*/
+/*Function 3: clear.onclick function which deletes the current note */
